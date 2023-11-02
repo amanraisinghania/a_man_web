cb9990c13b10b488c8de4ff4d49c553f305a35f4
amanraisinghania <amanraishinghania@gmail.com>
2023-11-02 17:31:18 +0530

    hggh

--- src/app/page.js
+++ src/app/page.js
@@ -1,244 +1,303 @@
 "use client";
-
-import Image from 'next/image'
-import style from "./home.module.css"
-import bg from "./bg.png"
+import { getDatabase, ref, onValue } from "firebase/database";
+import Image from "next/image";
+import style from "./home.module.css";
+import bg from "./bg.png";
 import { BsWhatsapp, BsMessenger } from "react-icons/bs";
 import { HiOutlineStatusOnline } from "react-icons/hi";
-import { AiFillStar ,AiFillSetting,AiOutlineMenu} from "react-icons/ai";
+import { AiFillStar, AiFillSetting, AiOutlineMenu } from "react-icons/ai";
 import { CgProfile } from "react-icons/cg";
 import { BiNotepad } from "react-icons/bi";
-import { useState } from 'react';
-
+import { useEffect, useState } from "react";
+import { app } from "../firebase";
 
 export default function Home() {
+  const [name, setName] = useState("");
 
-  const [data,setData] = useState([
-    { 
-      msg:"Hi!",
-      user:"aman"
+  function read() {
+    const db = getDatabase(app);
+    const starCountRef = ref(db, "name");
+    onValue(starCountRef, (snapshot) => {
+      const data = snapshot.val();
+      setName(data);
+      console.log("data => ", data);
+    });
+  }
+
+  useEffect(() => {
+    read();
+  }, []);
+
+  const [data, setData] = useState([
+    {
+      msg: "Hi!",
+      user: "aman",
     },
     {
-      msg:"Hello!",
-      user:"vijay"
+      msg: "Hello!",
+      user: "vijay",
     },
     {
-      msg:"How are you!",
-      user:"aman"
-  
+      msg: "How are you!",
+      user: "aman",
     },
     {
-      msg:"i am fine.",
-      user:"vijay"
-    },
-    { 
-      msg:"Hi!",
-      user:"aman"
+      msg: "i am fine.",
+      user: "vijay",
     },
     {
-      msg:"Hello!",
-      user:"vijay"
+      msg: "Hi!",
+      user: "aman",
     },
     {
-      msg:"How are you!",
-      user:"aman"
-  
+      msg: "Hello!",
+      user: "vijay",
     },
     {
-      msg:"i am fine.",
-      user:"vijay"
-    },
-    { 
-      msg:"Hi!",
-      user:"aman"
+      msg: "How are you!",
+      user: "aman",
     },
     {
-      msg:"Hello!",
-      user:"vijay"
+      msg: "i am fine.",
+      user: "vijay",
     },
     {
-      msg:"How are you!",
-      user:"aman"
-  
+      msg: "Hi!",
+      user: "aman",
     },
     {
-      msg:"i am fine.",
-      user:"vijay"
-    },
-    { 
-      msg:"Hi!",
-      user:"aman"
+      msg: "Hello!",
+      user: "vijay",
     },
     {
-      msg:"Hello!",
-      user:"vijay"
+      msg: "How are you!",
+      user: "aman",
     },
     {
-      msg:"How are you!",
-      user:"aman"
-  
+      msg: "i am fine.",
+      user: "vijay",
     },
     {
-      msg:"i am fine.",
-      user:"vijay"
-    },
-    { 
-      msg:"Hi!",
-      user:"aman"
+      msg: "Hi!",
+      user: "aman",
     },
     {
-      msg:"Hello!",
-      user:"vijay"
+      msg: "Hello!",
+      user: "vijay",
     },
     {
-      msg:"How are you!",
-      user:"aman"
-  
+      msg: "How are you!",
+      user: "aman",
     },
     {
-      msg:"i am fine.",
-      user:"vijay"
-    }
+      msg: "i am fine.",
+      user: "vijay",
+    },
+    {
+      msg: "Hi!",
+      user: "aman",
+    },
+    {
+      msg: "Hello!",
+      user: "vijay",
+    },
+    {
+      msg: "How are you!",
+      user: "aman",
+    },
+    {
+      msg: "i am fine.",
+      user: "vijay",
+    },
   ]);
 
-const list = [
-  {people:"aman"},
-   {people:"aman 1"},
-    {people:"aman 2"},
-     {people:"aman 3"},
-     {people:"aman 4"},
-     {people:"aman 5"},
-     {people:"aman 6"},
-     {people:"aman 7"},
-     {people:"aman 8"},
-     {people:"aman 9"},
-     {people:"aman 0"},
-]
+  const list = [
+    { people: "aman" },
+    { people: "aman 1" },
+    { people: "aman 2" },
+    { people: "aman 3" },
+    { people: "aman 4" },
+    { people: "aman 5" },
+    { people: "aman 6" },
+    { people: "aman 7" },
+    { people: "aman 8" },
+    { people: "aman 9" },
+    { people: "aman 0" },
+  ];
 
+  const [msgInput, setMsgInput] = useState("Inoutd asd");
 
-function addChat(){
-  setData(prev=> [...prev,{ msg:"Vijay",
-  user:"aman"}] )
-}
+  function addChat() {
+    setData((prev) => [
+      ...prev,
+      {
+        msg: msgInput,
+        user: "aman",
+      },
+    ]);
+  }
 
   return (
-    
-<>
-
-
-
-    <div style={{
-      overflow:"hidden"
-    }}>
-      <div className={style.upper}><BsWhatsapp color='white' size={30}/></div>      
-    
-      <div className={style.main}>
-        <div className={style.side}>
-          <div><BsMessenger color='white' size={30}/>
-          <HiOutlineStatusOnline color='white' size={30}/></div>
-          <div><AiFillStar color='white' size={30}/>
-          <AiFillSetting  color='white' size={30}/>
-          <CgProfile  color='white' size={30}/></div>
+    <>
+      <div
+        style={{
+          overflow: "hidden",
+          padding: 10,
+          display: "flex",
+          gap: 10,
+          height: "100vh",
+          width: "100vw",
+          background: "black",
+        }}
+      >
+        {/* Left Side Bar */}
+        <div className={style.sideBar}>
+          <div>
+            <BsMessenger color="#808080" size={30} />
+            <HiOutlineStatusOnline color="#808080" size={30} />
           </div>
-        
-        <div className={style.bodymain}>
-              {/* Left Side */}
-             <div className={style.body}>
-                <div className={style.chat}>chat 
-                  <div style={{
-                    display:"flex",
-                  }}>
-                  <BiNotepad size={30}/>
-                    <AiOutlineMenu size={30}/>
-                  </div>
-                </div>
-                <div className={style.input}>
-                  <input type="text" placeholder="chat"></input>
-                </div>
-                  {/* // Chat Head */}
-                  <div className={style.sting}>
-                
-                            {
-                              list.map((e)=>{
-                                return(
-                              <div key={e} className={style.empo} style={{
-                                display:"flex",
-                                gap:10,
-                                alignItems:"center",
-                                color:"white",
-                              }}> 
-                              <Image src="/1.jpg" width={50} height={50} style={{
-                                borderRadius:50,
-                                  aspectRatio:1,
-                              }} />
-                                <h1>hi</h1>
-                                  </div>
-                                )
-                              })
-                            }
-                        
-                  </div>
-                      {/* // Chat Head Close */}
-              </div>
-              {/* Left Side close */}
-
-
-              {/* Right Side */}
-
-             
-
-                <div>
-                <div className={style.topmain}>
-                <div className={style.top}><Image src="/1.jpg" width={50} height={50}/>
-                <div className={style.mess}>hi</div></div>
-              </div>
-                  <div className={style.body2} style={{
-                        backgroundImage: "url(" + bg.src + ")",
-                        display:"flex",
-                        flexDirection:"column",
-                        gap:10,
-                        padding:10,
-                         }}> 
-                        {data.map(function(a){
-                          return (
-                              a.user == "aman" ?  
-                              <div className={style.text}><span className={style.mess}>{a.msg}</span></div> :
-                              <div className={style.text2}><span className={style.mess2}>hellodasdasjdhghdashdsahdashdghashasjhsdjashdgasdasdkashdjashkjash</span>          
-                              </div>
-                          )
-                        })}
-               
-                  </div>
-                  <div>
-                    <div style={{
-                      display:"flex",
-                      padding:10
-                    }}>
-                    <input type="text" placeholder="text your mess" 
-                    onClick={()=>alert("hi")} style={{
-                      marginBottom:10,
-                      flex:1
-                      }}></input>
-                      <div style={{
-                        color:"white",
-                      }}>
-                        <button style={{
-                          marginRight:40,
-                          background:"yellow",
-                          color:"black",
-                          padding:5,
-                          paddingLeft:20,
-                          paddingRight:20,
-                        }} onClick={()=>addChat()}>send</button></div>
-                      
-                      </div>
-                  </div>
-                </div>
+          <div>
+            <AiFillStar color="#808080" size={30} />
+            <AiFillSetting color="#808080" size={30} />
+            <CgProfile color="#808080" size={30} />
           </div>
- 
         </div>
+        {/* Left Side Bar Ends */}
 
+        {/* Left Chat Side close */}
+        <div className={style.bodyLeft}>
+          <div className={style.chat}>User Name</div>
+          <input className={style.input} type="text" placeholder="chat" />
+          {/* // Chat Head */}
+          <div className={style.sting}>
+            {list.map((e) => {
+              return (
+                <div
+                  key={e}
+                  className={style.empo}
+                  style={{
+                    display: "flex",
+                    gap: 10,
+                    alignItems: "center",
+                    color: "white",
+                  }}
+                >
+                  <Image
+                    src="/1.jpg"
+                    width={50}
+                    height={50}
+                    style={{
+                      borderRadius: 50,
+                      aspectRatio: 1,
+                    }}
+                  />
+                  <h1>hi</h1>
+                </div>
+              );
+            })}
+          </div>
+          {/* // Chat Head Close */}
+        </div>
+        {/* Left Side close */}
+
+        {/* Right Chat Side */}
+        <div
+          style={{
+            height: "100%",
+            backgroundImage: "url(" + bg.src + ")",
+            flex: 1,
+            display: "flex",
+            flexDirection: "column",
+            gap: 5,
+            borderRadius: 10,
+          }}
+          className={style.bodyRight}
+        >
+          {/* Top Bar */}
+          <div className={style.topmain}>
+            <Image
+              src="/1.jpg"
+              width={40}
+              height={40}
+              style={{
+                borderRadius: 50,
+                margin: 5,
+              }}
+            />
+            <p
+              style={{
+                marginTop: "auto",
+                marginBottom: "auto",
+                color: "white",
+              }}
+            >
+              Aman
+            </p>
+          </div>
+
+          {/* All Chats */}
+          <div
+            className={style.body2}
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              gap: 10,
+              padding: 10,
+            }}
+          >
+            {data.map(function (a) {
+              return a.user == "aman" ? (
+                <div className={style.text}>
+                  <span className={style.mess}>{a.msg}</span>
+                </div>
+              ) : (
+                <div className={style.text2}>
+                  <span className={style.mess2}>
+                    hellodasdasjdhghdashdsahdashdghashasjhsdjashdgasdasdkashdjashkjash
+                  </span>
+                </div>
+              );
+            })}
+          </div>
+
+          {/* Send Button */}
+          <div
+            style={{
+              display: "flex",
+              marginBottom: 5,
+              marginLeft: 5,
+              marginRight: 5,
+              gap: 5,
+            }}
+          >
+            <input
+              type="text"
+              placeholder="text your mess"
+              value={msgInput}
+              onChange={(e) => setMsgInput(e.target.value)}
+              style={{
+                flex: 1,
+                borderRadius: 5,
+                paddingLeft: 10,
+              }}
+            />
+
+            <button
+              style={{
+                background: "yellow",
+                color: "black",
+                padding: 5,
+                paddingLeft: 20,
+                paddingRight: 20,
+                borderRadius: 5,
+              }}
+              onClick={() => addChat()}
+            >
+              send
+            </button>
+          </div>
+        </div>
       </div>
-      </>    
-  )
+    </>
+  );
 }
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBLQ99n_7zdR46klQDwst2S24pl4yMO5vI",
+  authDomain: "aman-web-990b6.firebaseapp.com",
+  projectId: "aman-web-990b6",
+  storageBucket: "aman-web-990b6.appspot.com",
+  messagingSenderId: "927590384295",
+  appId: "1:927590384295:web:233bfd00d3712d89a89182"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
